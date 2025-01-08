51310b47cfc6b274a7fa20573cb2d123dddb422d
fbablu <fardeeneb@gmail.com>
2025-01-08 02:00:08 -0600

    deploy vabs page

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/',
   build: {
-    outDir: 'dist',
-    assetsDir: '',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    outDir: '',
+    emptyOutDir: false
   }
 })
