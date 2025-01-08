7823a0e85bd11cd579d79bd913dfdd8bb80cee74
fbablu <fardeeneb@gmail.com>
2025-01-08 01:47:43 -0600

    deploying to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
